b28bd771046938ec42f784bd73495636c8fe6aab
BeH4u9K <denis-nejivl9k@yandex.ru>
2024-04-07 12:45:00 +0300

    feat: add gh-pages

--- src/gracass/Gracasa.jsx
+++ src/gracass/Gracasa.jsx
@@ -21,12 +21,13 @@ const Gracasa = () => {
                         </h2>
                     </div>
 
-                    <form className='DivButton' action="https://www.youtube.com/watch?v=Chu4fCCBylk&t=3304s&ab_channel=%D0%94%D0%96%D0%90%D0%A0%D0%90%D0%A5%D0%9E%D0%92" target="_blank">
+                    <form className='DivButton' action="https://t.me/Gracesa_bot" target="_blank">
                         <button className='Button'>Перейти</button>
                     </form>
                 </div>
             </div>
         </div>
+        
     );
 };
 
